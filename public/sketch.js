c231319e3152a6f194a84dc9b770b0cc049205b4
Glitch (glitch-hello-website) <none>
2025-04-02 12:46:37 +0200

    second

--- public/sketch.js
+++ public/sketch.js
@@ -1,18 +1,15 @@
 let socket;
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(200);
     socket = io();
-
     socket.on('ellipse', (data) => {
         fill(data.color);
         ellipse(data.x, data.y, 50, 50);
     });
-
 }
 
-
-//
 
 ///
 function windowResized() {
